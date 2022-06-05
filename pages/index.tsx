f80a2430f45261352adbbc07f71ed50a35f0725c
buzuosheng <1015350043@qq.com>
2022-06-05 16:34:30 +0800

    tailwind @apply

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <button className="btn-blue">button</button>
+    </div>
+  )
+}
+
+export default Home
